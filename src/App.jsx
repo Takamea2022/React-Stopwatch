e9284a0f2d7e8405e70851dd97e95ac8a69eeea1
Takamea2022 <arante.emmanuel2017.com>
2024-08-01 23:49:35 +0800

    Stopwatch in React

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Stopwatch from "./Stopwatch.jsx";
+
+
+
+function App() {
+
+    return(
+      <>
+      
+        <Stopwatch />
+        <Stopwatch />
+        <Stopwatch />
+
+      </>
+    
+    )
+}
+
+export default App
+  
